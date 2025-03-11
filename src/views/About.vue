d8d659d6fe8aecb0138e4070a5579a172c736c30
Marcomagnano93 <marcog.magnano@gmail.com>
2025-03-11 10:13:28 +0100

    ADD: about

--- src/views/About.vue
+++ src/views/About.vue
@@ -12,11 +12,21 @@ export default {
 </script>
 
 <template>
-<div class="container">
+<div class="container my-3">
   <h2>Chi sono</h2>
   <div class="row">
-    <div class="col">
-      <p></p>
+    <div class="col my-2">
+      <p>Nato a Torino nel 1993, ho sempre vissuto fuori città. Da sempre appassionato di tecnologia e cucina, mi reputo una persona creativa e dinamica.
+        Finiti gli studi ho iniziato a lavorare come cuoco, prima in Francia e poi a Malta, dove ho preso la certificazione in inglese B2.
+      </p>
+      <p>
+        Nel 2017 ho aperto una mia attività: una cioccolateria artigianale con annesso punto vendita. Durante questa esperienza, ho portato avanti la mia passione per la tecnologia e la programmazione, inizialmente con strumenti di grafica e gestione socialmedia.
+        Nel 2021 ho cessato l'attività dopo la crisi della pandemia. 
+        Ho ricominciato subito a lavorare come responsabile di cucina continuando a portare avanti la passione per la tecnologia da autodidatta fino a gennaio 2024, quando ho lasciato il lavoro per frequentare un corso intensivo di programmazione web.
+      </p>
+      <p>
+        Finito il corso, ho iniziato a lavorare come full-stack web developer presso Econica, dove ho approfondito anche la programmazione mobile, con Flutter e Dart, gestendo in prima persona progetti di alcuni clienti.
+      </p>
     </div>
   </div>
 </div>
